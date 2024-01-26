cd05b0ab1d623fbea18fd1321d15da2364d9a948
Jorge Tabuenca <jorgetabuenca99@gmail.com>
2024-01-26 13:11:49 +0100

    Initial commit

---
+++ src/main/java/com/items/ItemsPoC/ItemsPoCApplication.java
@@ -0,0 +1,13 @@
+package com.items.ItemsPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsPoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/items/ItemsPoC/ItemsPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.items.ItemsPoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
