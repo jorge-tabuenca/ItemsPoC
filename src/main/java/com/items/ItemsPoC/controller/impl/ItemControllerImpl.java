8b858349c128951f92e9d593a375e75b3d728169
Jorge Tabuenca <jorgetabuenca99@gmail.com>
2024-01-26 16:48:51 +0100

    [Pre-2] Connection to Swagger

--- src/main/java/com/items/ItemsPoC/controller/impl/ItemControllerImpl.java
+++ src/main/java/com/items/ItemsPoC/controller/impl/ItemControllerImpl.java
@@ -1,7 +1,10 @@
 package com.items.ItemsPoC.controller.impl;
 
 import com.items.ItemsPoC.controller.ItemController;
+import com.items.ItemsPoC.dto.ItemDto;
+import com.items.ItemsPoC.entity.ItemEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,15 @@ public class ItemControllerImpl implements ItemController {
     @GetMapping("/saludar")
     public String saluda() { return "Hola Mundo"; }
 
+    @Override
+    @GetMapping("/item/{itemId}")
+    public ItemEntity getItem(short itemId) throws Exception {
+        return null;
+    }
+
+    @Override
+    @PostMapping("/item")
+    public ItemEntity createItem(ItemDto itemDto) throws Exception {
+        return null;
+    }
 }
